d81c6d5597960628b889b74f2473b1cc1a48ed3d
LuanAccioly <acciolyluan@gmail.com>
2022-03-17 21:55:17 -0300

    questao 1 semana5

--- semana5/q1.hs
+++ semana5/q1.hs
@@ -20,5 +20,34 @@
     OBS: para definir um valor do tipo bloco é só especificar (Bloco ind trs prox), onde ind é um inteiro, trs é uma lista de transações e prox é um Maybe Bloco. O mesmo se aplica ao tipo Transacao.
 -}
 
+data Transacao = Transacao String String Float deriving (Show)
+data Bloco = Bloco Int [Transacao] (Maybe Bloco) deriving (Show)
+
+--Função principal
+indiceVolume :: Bloco -> Int
+indiceVolume a = (verificador a a)
+
+mostraBloco:: Bloco -> Bloco -> Int
+mostraBloco (Bloco x y (Just(z))) (Bloco a b (Nothing)) | (volume y) >= (volume b) = x
+                                                        | otherwise = a                                                        
+mostraBloco (Bloco x y (Just(z))) (Bloco a b (Just(c))) | (volume y) >= (volume b) = x 
+                                                        | otherwise = a
+
+volume :: [Transacao] -> Float
+volume [] = 0.0
+volume (x:xs) = (pegaValor x) + (volume xs)
+
+pegaValor :: Transacao -> Float
+pegaValor (Transacao x y z) = z
+
+valor (Just x) = x
+
+verificador :: Bloco -> Bloco -> Int
+verificador x (Bloco a b (Nothing)) = mostraBloco x (Bloco a b (Nothing))
+verificador x (Bloco a b (Just (c)))  | (mostraBloco x (Bloco a b (Just (c)))) == a = verificador (Bloco a b (Just (c))) c
+                                                           | otherwise = verificador x c
+
+
+mostraBloco (Bloco 1 [(Transacao "123" "321" 54.5)] (Just ((Bloco 2 [(Transacao "123" "321" 100.0)] Nothing)))) (Bloco 2 [(Transacao "123" "321" 1.0)] Nothing)
 
 
