d56a84dbe209093c01605895f46c413aed5d6ae6
LuanAccioly <acciolyluan@gmail.com>
2022-03-24 21:33:53 -0300

    adicionando comentarios; permitindo que se receba nomes completos

--- semana6/q1.hs
+++ semana6/q1.hs
@@ -16,6 +16,20 @@
     - média de pessoas que coabitam com pessoas com diagnóstico ou suspeita de infecção por COVID-19.
 -}
 
+{-
+    Explicando como funciona;
+    ================================================================
+    A função relatorioCovid é a que vamos rodar;
+    Ao rodar, pode colocar nomes completos e apertar enter;
+    Após o nome será perguntado se a pessoa mencionada está com covid e a resposta deve ser o caracter s ou n;
+    Mesma coisa com a pergunta se a pessoa coabita com pessoas infectadas;
+    Repetir o processo até enjoar;
+    Ao enjoar, quando chegar na pergunta sobre o nome é só apertar ENTER, sem espaços;
+    Um arquivo auxiliar chamado relatorio.txt e um outro chamado covid.txt serão criados na raiz de onde está o arquivo;
+    Abra o arquivo covid.txt;
+    ================================================================
+-}
+
 import Data.List
 
 relatorioCovid = do 
@@ -30,15 +44,15 @@ relatorioCovid = do
                     appendFile "relatorio.txt" ( nome ++ " " ++ confirmadoCovid ++ " " ++ coabitaInfectados ++ "\n")
                     relatorioCovid
 
-        else aux    
+        else finalizaArquivo    
 
 mediaInfectados :: String -> Float
-mediaInfectados l = fromIntegral(length ([x | x <- (lines l), (isInfixOf "s s" x) || (isInfixOf "s n" x)])) / fromIntegral(totalRespostas l)
+mediaInfectados l = fromIntegral(length ([x | x <- (lines l), (isInfixOf " s s" x) || (isInfixOf " s n" x)])) / fromIntegral(totalRespostas l)
 
 mediaConvivio :: String -> Float
-mediaConvivio l = fromIntegral(length ([x | x <- (lines l), (isInfixOf "s s" x) || (isInfixOf "n s" x)])) / fromIntegral(totalRespostas l)
+mediaConvivio l = fromIntegral(length ([x | x <- (lines l), (isInfixOf " s s" x) || (isInfixOf " n s" x)])) / fromIntegral(totalRespostas l)
 
-aux = do 
+finalizaArquivo = do 
     c <- readFile "relatorio.txt"
     appendFile "covid.txt" ( c ++ "\nTotal respostas: " ++ (show (totalRespostas c)) ++ "\nMedia contaminados: " ++ (show(mediaInfectados c)) ++ "\nmedia pessoas que coabitam: " ++ (show(mediaConvivio c)))
     putStrLn "As informacoes foram adicionadas ao arquivo covid.txt"
